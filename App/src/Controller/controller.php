a769ba4409a0ac0e5b16a0643eecf77996b1a4cd
Simon FALCONNET BANEYX <simon.falconnet-baneyx@etu.u-pec.fr>
2025-12-16 15:06:35 +0100

    update arborescence

--- App/src/Controller/controller.php
+++ App/src/Controller/controller.php
@@ -4,7 +4,7 @@
 class controller{
 
     public static function UsePage(string $Vue) : void {
-        require __DIR__."/view/$Vue"; // Charge la vue
+        require __DIR__."../src/view/$Vue"; // Charge la vue
     }
      
 }
